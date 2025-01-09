28a5d8815fa248fd96caf07b0123dfa580db43ea
lbastoss <bastosls17@gmail.com>
2025-01-09 16:04:03 -0300

    instalando e configurando biomejs

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
-import { Router } from 'express'
+import { Router } from "express"
 const routes = new Router()
 
-routes.get('/', (req, res) => {
-    return res.status(200).json({ message: 'Hello World!' })
+routes.get("/", (req, res) => {
+	return res.status(200).json({ message: "Hello World!" })
 })
 
 export default routes
