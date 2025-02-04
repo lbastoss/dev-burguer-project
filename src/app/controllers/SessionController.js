09408aa1e39e10975a8635f8ed5afd8b5a8ad915
lbastoss <bastosls17@gmail.com>
2025-02-04 15:55:37 -0300

    criando a comparação com o banco de dados no cadastro de usuarios

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,6 +1,47 @@
+import * as Yup from 'yup'
+import User from '../models/User'
+
 class SessionController {
 	async store(req, res) {
-		return res.json({ message: 'session' })
+		const schema = Yup.object({
+			email: Yup.string().email().required(),
+			password: Yup.string().min(6).required(),
+		})
+
+		const isValid = await schema.isValid(req.body)
+
+		const emailOrPAsswordIncorrect = () => {
+			res.status(401).json({ error: 'Make sure your email or password are correct' })
+		}
+
+		if (!isValid) {
+			return emailOrPAsswordIncorrect()
+		}
+
+		const { email, password } = req.body
+
+		const user = await User.findOne({
+			where: {
+				email,
+			},
+		})
+
+		if (!user) {
+			return emailOrPAsswordIncorrect()
+		}
+
+		const isSamePassword = await user.checkPassword(password)
+
+		if (!isSamePassword) {
+			return emailOrPAsswordIncorrect()
+		}
+
+		return res.status(201).json({
+			id: user.id,
+			name: user.name,
+			email,
+			admin: user.admin,
+		})
 	}
 }
 
