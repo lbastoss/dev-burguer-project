ebf07126ce6c1eae5969dd01c2b4d505d362cae8
lbastoss <bastosls17@gmail.com>
2025-02-04 17:18:19 -0300

    criando controller de produtos

---
+++ src/app/controllers/ProductController.js
@@ -0,0 +1,21 @@
+import * as Yup from 'yup'
+
+class ProductController {
+	async store(req, res) {
+		const schema = Yup.object({
+			name: Yup.string().required(),
+			price: Yup.number().required(),
+			category: Yup.string().required(),
+		})
+
+		try {
+			schema.validateSync(req.body, { abortEarly: false })
+		} catch (err) {
+			return res.status(400).json({ error: err.errors })
+		}
+
+		return res.status(201).json({ message: 'ok' })
+	}
+}
+
+export default new ProductController()
