244996af706abab4ce5e7c79ac24de36e05c8aeb
lbastoss <bastosls17@gmail.com>
2025-02-26 17:04:44 -0300

    fazendo alterações para update de categorias e finalizando o backend

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -20,6 +20,7 @@ class CategoryController {
 			return res.status(401).json()
 		}
 
+		const { filename: path } = req.file
 		const { name } = req.body
 
 		const categoryExists = await Category.findOne({
@@ -34,9 +35,70 @@ class CategoryController {
 
 		const { id } = await Category.create({
 			name,
+			path,
 		})
 
 		return res.status(201).json({ id, name })
+	}
+
+	async update(req, res) {
+		const schema = Yup.object({
+			name: Yup.string(),
+		})
+
+		try {
+			schema.validateSync(req.body, { abortEarly: false })
+		} catch (err) {
+			return res.status(400).json({ error: err.errors })
+		}
+
+		const { admin: isAdmin } = await User.findByPk(req.userId)
+
+		if (!isAdmin) {
+			return res.status(401).json()
+		}
+
+		const { id } = req.params
+
+		const categoryExists = await Category.findByPk(id)
+
+		if (!categoryExists) {
+			return res.status(400).json({ message: 'Make sure your category ID is correct' })
+		}
+
+		let path
+
+		if (req.file) {
+			path = req.file.filename
+		}
+
+		const { name } = req.body
+
+		if (name) {
+			const categoryNameExists = await Category.findOne({
+				where: {
+					name,
+				},
+			})
+
+			if (categoryNameExists && categoryNameExists.id !== +id) {
+				return res.status(400).json({ error: 'Category already exists' })
+			}
+		}
+
+		await Category.update(
+			{
+				name,
+				path,
+			},
+			{
+				where: {
+					id,
+				},
+			},
+		)
+
+		return res.status(200).json()
 	}
 
 	async index(req, res) {
