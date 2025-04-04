c8395571821afaa4a13f6d6f35964fa4c25ef2fc
lbastoss <bastosls17@gmail.com>
2025-04-04 15:59:38 -0300

    atualizando o banco de dados com os produtos e categorias

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -36,7 +36,7 @@ class UserController {
 		})
 
 		if (userExists) {
-			return res.status(400).json({ error: 'User already exists' })
+			return res.status(409).json({ error: 'User already exists' })
 		}
 
 		const user = await User.create({
