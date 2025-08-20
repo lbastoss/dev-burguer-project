692d2e3571bc5e47d02d4e7c8c8b5710699624d9
lbastoss <lucas.bastos.7@hotmail.com>
2025-08-20 18:59:24 -0300

    correção de bugs

--- src/app.js
+++ src/app.js
@@ -1,28 +1,28 @@
-import express from 'express'
-import routes from './routes'
-import './database'
-import { resolve } from 'node:path'
-import cors from 'cors'
+import express from 'express';
+import routes from './routes';
+import './database';
+import { resolve } from 'node:path';
+import cors from 'cors';
 
 class App {
 	constructor() {
-		this.app = express()
+		this.app = express();
 
-		this.app.use(cors())
-		this.middlewares()
-		this.routes()
+		this.app.use(cors());
+		this.middlewares();
+		this.routes();
 	}
 
 	middlewares() {
-		this.app.use(express.json())
-		this.app.use('/product-file', express.static(resolve(__dirname, '..', 'uploads')))
+		this.app.use(express.json());
+		this.app.use('/product-file', express.static(resolve(__dirname, '..', 'uploads')));
 
-		this.app.use('/category-file', express.static(resolve(__dirname, '..', 'uploads')))
+		this.app.use('/category-file', express.static(resolve(__dirname, '..', 'uploads')));
 	}
 
 	routes() {
-		this.app.use(routes)
+		this.app.use(routes);
 	}
 }
 
-export default new App().app
+export default new App().app;
